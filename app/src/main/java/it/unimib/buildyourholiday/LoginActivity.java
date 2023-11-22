0e9890e31261ef56e7a22134a81dcfe5f7788125
Luca Mariani <l.mariani55@campus.unimib.it>
2023-11-22 15:33:30 +0100

    controllo password e mail base

--- app/src/main/java/it/unimib/buildyourholiday/LoginActivity.java
+++ app/src/main/java/it/unimib/buildyourholiday/LoginActivity.java
@@ -1,14 +1,98 @@
 package it.unimib.buildyourholiday;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textInputLayoutEmail = findViewById(R.id.textInputLayout_email);
+        textInputLayoutPassword = findViewById(R.id.textInputLayout_password);
+        final Button buttonLogin = findViewById(R.id.button_login);
+
+        buttonLogin.setOnClickListener(v -> {
+
+            String email = textInputLayoutEmail.getEditText().getText().toString();
+            String password = textInputLayoutPassword.getEditText().getText().toString();
+
+            // Start login if email and password are ok
+            if (isEmailOk(email) & isPasswordOk(password)) {
+                Log.d(TAG, "Email and password are ok");
+                //saveLoginData(email, password);
+            } else {
+                Snackbar.make(findViewById(android.R.id.content),
+                        "Check your data", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+
+    //controllo base email e password
+    private boolean isEmailOk(String email) {
+        // Check if the email is valid through the use of this library:
+        // https://commons.apache.org/proper/commons-validator/
+        if (!EmailValidator.getInstance().isValid((email))) {
+            textInputLayoutEmail.setError(getString(R.string.error_email));
+            return false;
+        } else {
+            textInputLayoutEmail.setError(null);
+            return true;
+        }
+    }
+
+    /**
+     * Checks if the password is not empty.
+     * @param password The password to be checked
+     * @return True if the password is not empty, false otherwise
+     */
+    private boolean isPasswordOk(String password) {
+        // Check if the password length is correct
+        if (password.isEmpty() || password.length() < 8) {
+            textInputLayoutPassword.setError(getString(R.string.error_password));
+            return false;
+        } else {
+            textInputLayoutPassword.setError(null);
+            return true;
+        }
+    }
+
+
+
+
+
 }
