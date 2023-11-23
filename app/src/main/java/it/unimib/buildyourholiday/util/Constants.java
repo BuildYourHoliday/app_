47c0ef88765346bb173681c3f888d88748c3386d
Luca Mariani <l.mariani55@campus.unimib.it>
2023-11-23 18:56:45 +0100

    util package with constants e dataEncryption + LoginActivity update with dataEncryption + nav_profile per fragment del profilo

---
+++ app/src/main/java/it/unimib/buildyourholiday/util/Constants.java
@@ -0,0 +1,16 @@
+package it.unimib.buildyourholiday.util;
+
+/**
+ * Utility class to save constants used by the app.
+ */
+public class Constants {
+
+
+    // Constants for EncryptedSharedPreferences
+    public static final String ENCRYPTED_SHARED_PREFERENCES_FILE_NAME = "it.unimib.worldnews.encrypted_preferences";
+    public static final String EMAIL_ADDRESS = "email_address";
+    public static final String PASSWORD = "password";
+
+    // Constants for encrypted files
+    public static final String ENCRYPTED_DATA_FILE_NAME = "it.unimib.worldnews.encrypted_file.txt";
+}
