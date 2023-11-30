2c6fb23792d55d625f5b174e4a94f053a1591a21
Manuel Bosisio <m.bosisio19@campus.unimib.it>
2023-11-30 23:56:09 +0100

    added MapBox dependencies, map fragment first setup

--- app/src/main/java/it/unimib/buildyourholiday/MapFragment.java
+++ app/src/main/java/it/unimib/buildyourholiday/MapFragment.java
@@ -1,12 +1,28 @@
 package it.unimib.buildyourholiday;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.mapbox.common.ResourceLoadOptions;
+import com.mapbox.maps.*;
+import com.mapbox.maps.ResourceOptions;
+import com.mapbox.maps.ResourceOptionsManager;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.loader.MapboxMaps;
+import com.mapbox.maps.loader.MapboxMapsInitializer;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +39,8 @@ public class MapFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private MapView mapView = null;
 
     public MapFragment() {
         // Required empty public constructor
@@ -53,12 +71,50 @@ public class MapFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        // GeoJSONObject geoJSONObject = GeoJSON.parse();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ResourceOptionsManager.Companion.getDefault(getContext().getApplicationContext(),
+                getString(R.string.mapbox_access_token));
+        Log.d("MMMMMMMMMMMMMM","dopo GETINSTANCE() + "+getString(R.string.mapbox_access_token));
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+
+        mapView = view.findViewById(R.id.map_view);
+        mapView.getMapboxMap().loadStyleUri(Style.LIGHT);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.internal.impldep.org.jsoup.safety.Safelist.basic
+import java.net.URI
+import java.net.URL
+
 pluginManagement {
     repositories {
         google()
@@ -10,6 +14,19 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = (URI("https://api.mapbox.com/downloads/v2/releases/maven"))
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                // Do not change the username below.
+                // This should always be `mapbox` (not your username).
+                username = "mapbox"
+                // Use the secret token you stored in gradle.properties as the password
+                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            }
+        }
     }
 }
 
